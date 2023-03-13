7251396f2ad52f0dff3b7ef2d548ff576dedf8fa
Bryan Pablo <bryanpablo472@gmail.com>
2023-03-13 11:57:26 -0700

    finished putting it together assingment

---
+++ my-app/src/App.js
@@ -0,0 +1,38 @@
+
+import "./App.css";
+import PersonCard from "./components/PersonCard";
+
+const peopleArr = [
+  {
+    firstName: "Doe",
+    lastName: "Jane",
+    age: 45,
+    hairColor: "Black",
+  },
+  {
+    firstName: "Smith",
+    lastName: "John",
+    age: 88,
+    hairColor: "Brown",
+  },
+ 
+];
+
+function App() {
+  return (
+    <div className="App">
+      {peopleArr.map((personObj, index) => (
+        <PersonCard
+          key={index}
+          firstName={personObj.firstName}
+          lastName={personObj.lastName}
+          age={personObj.age}
+          hairColor={personObj.hairColor}
+        />
+      ))}
+     
+    </div>
+  );
+}
+
+export default App;
